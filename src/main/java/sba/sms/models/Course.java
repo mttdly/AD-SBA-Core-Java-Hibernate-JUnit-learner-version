02bbbf8171dd694670ec5b61efe7530ab3760982
Matthew <matthewaarondaly@gmail.com>
2023-01-26 16:21:47 -0500

    Course model near completed

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,90 @@
 package sba.sms.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter
+@Getter
+@ToString
+@Table(name = "course")
+@Entity
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK
+    @Column(name = "id")
+    int id;
+
+    @NonNull
+    @Column(name = "name", length = 50) 
+    String name;
+
+    @NonNull
+    @Column(name = "instructor", length = 50)
+    String instructor;
+
+    @ToString.Exclude // Exclude collections to avoid infinite loop
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, mappedBy = "courses")
+    List<Student> students;
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Course other = (Course) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (instructor == null) {
+            if (other.instructor != null)
+                return false;
+        } else if (!instructor.equals(other.instructor))
+            return false;
+        if (students == null) {
+            if (other.students != null)
+                return false;
+        } else if (!students.equals(other.students))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((instructor == null) ? 0 : instructor.hashCode());
+        result = prime * result + ((students == null) ? 0 : students.hashCode());
+        return result;
+    }
 }
