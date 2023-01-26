aff51b7a2ed98a490779d36383a407a9b6be31b3
Matthew <matthewaarondaly@gmail.com>
2023-01-26 14:20:11 -0500

    Save progress

--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,49 @@
 package sba.sms.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter
+@Getter
+@ToString
+
+@Table(name = "student")
+@Entity
 public class Student {
+    @Id // Makes it PK
+    @Column(length = 50, name = "email")
+    String email;
+
+    @Column(length = 50, nullable = false, name = "name")
+    String name;
+
+    @Column(length = 50, nullable = false, name = "password")
+    String password;
+
+    @ToString.Exclude
+
+    // Student Database Attributes
+    @JoinTable(name = "student_courses",
+            pkColumn = @JoinColum(name = "student_email"),
+            inversePK = @JoinColumn(name = "courses_id"))
+
+    // A student can have many courses, and a course can have many students
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+
+
+
 
 }
--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,5 +1,34 @@
 package sba.sms.services;
 
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.logging.Logger;
+
+import sba.sms.models.Course;
+import sba.sms.models.Student;
+
 public class StudentService  {
+
+    // public void registerStudentToCourse(String email, int i) {
+    // }
+
+    // public Logger getStudentByEmail(String email) {
+    //     return null;
+    // }
+
+    // public void createStudent(Student student) {
+    // }
+
+    // public List<Course> getStudentCourses(String email) {
+    //     return null;
+    // }
+
+    // public boolean validateStudent(String email, String password) {
+    //     return false;
+    // }
+
+    // public IntPredicate getAllStudents() {
+    //     return null;
+    // }
 
 }
