f02e68fe78bbd26185998e99bee060d41db50196
Matthew <matthewaarondaly@gmail.com>
2023-01-26 17:05:59 -0500

    Swap validations, fix JoinTable, add hashc/equals

--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,8 +1,15 @@
 package sba.sms.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +18,6 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.FieldDefaults;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -27,23 +33,64 @@ public class Student {
     @Column(length = 50, name = "email")
     String email;
 
-    @Column(length = 50, nullable = false, name = "name")
+    @NonNull
+    @Column(length = 50, name = "name")
     String name;
 
-    @Column(length = 50, nullable = false, name = "password")
+    @NonNull
+    @Column(length = 50, name = "password")
     String password;
 
-    @ToString.Exclude
-
     // Student Database Attributes
+    // Connect tables
     @JoinTable(name = "student_courses",
-            pkColumn = @JoinColum(name = "student_email"),
-            inversePK = @JoinColumn(name = "courses_id"))
-
+            joinColumns = @JoinColumn(name = "student_email"),
+            inverseJoinColumns = @JoinColumn(name = "courses_id"))
     // A student can have many courses, and a course can have many students
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    @ToString.Exclude // prevent inf loop output of collections
+    List<Course> courses;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((courses == null) ? 0 : courses.hashCode());
+        return result;
+    }
 
-
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (courses == null) {
+            if (other.courses != null)
+                return false;
+        } else if (!courses.equals(other.courses))
+            return false;
+        return true;
+    }
 }
