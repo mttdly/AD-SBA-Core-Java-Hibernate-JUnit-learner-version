272dc1258518b6639348f2e09fe517ecbeb660bf
Matthew <matthewaarondaly@gmail.com>
2023-01-27 12:07:08 -0500

    finish createCourse impl

--- src/main/java/sba/sms/services/CourseService.java
+++ src/main/java/sba/sms/services/CourseService.java
@@ -5,26 +5,25 @@ import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import sba.sms.dao.CourseI;
 import sba.sms.models.Course;
 import sba.sms.utils.HibernateUtil;
 
-public class CourseService  implements CourseI {
+public class CourseService implements CourseI {
 
     @Override
     public void createCourse(Course course) {
-        // Open session
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = null;
-
         try {
             tx = session.beginTransaction();
             session.persist(course);
-            tx.commit();
-        } catch (HibernateException exception) {
-            // TODO: handle rollback
-            exception.printStackTrace();
+            tx.commit(); // Commit Handle
+        } catch (HibernateException e) { // Base type for Hibernate exceptions
+            if (tx != null) tx.rollback(); // Rollback Handle
+            e.printStackTrace();
         } finally {
             session.close();
         }
